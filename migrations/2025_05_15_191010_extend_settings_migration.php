1df436f82f81109b1d71f252511b3293382e8719
sirzento <sirzento@gmx.de>
2025-05-16 22:46:11 +0200

    changed default ssh port to 22

--- migrations/2025_05_15_191010_extend_settings_migration.php
+++ migrations/2025_05_15_191010_extend_settings_migration.php
@@ -11,7 +11,7 @@ return new class extends Migration
         $blueprint = app(BlueprintExtensionLibrary::class);
 
         $blueprint->dbSetMany("{identifier}", [
-            'port' => 20,
+            'port' => 22,
             'useSSHKey' => false,
         ]);
     }
